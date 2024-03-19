0444e9318a165657fe1fe902a7da7a27081f02da
Sophie Y <javachiphi@gmail.com>
2024-03-19 17:06:04 +0800

    turn chunks into embeds, store, similarity search

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from langchain.document_loaders import TextLoader
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores.chroma import Chroma
 from dotenv import load_dotenv
 
 
 load_dotenv()
+
+embeddings = OpenAIEmbeddings()
+emb = embeddings.embed_query("Hello")
+
 
 text_splitter = CharacterTextSplitter(
   separator="\n",
@@ -16,6 +22,19 @@ docs = loader.load_and_split(
   text_splitter=text_splitter
 )
 
-for doc in docs: 
-  print(doc.page_content)
+# turn chunks into embeddings and store them
+db = Chroma.from_documents(
+  docs,
+  embedding=embeddings,
+  persist_directory="emb"
+)
+
+# 
+
+results = db.similarity_search("what is an interesting fact about the English language?", k=1)
+
+
+
+for result in results: 
   print("\n")
+  print(result.page_content)
